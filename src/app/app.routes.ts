dce2fa310040d0ed1b1436fd9e6c4f45efe9b6e0
Leandro Andrade <leandro.andrade.401@ufrn.edu.br>
2025-12-03 14:51:06 -0300

    update of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MainBolsistaComponent } from './components/main-bolsista/main-bolsista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'meus-dados',
+    component: MainBolsistaComponent
+  }
+];
