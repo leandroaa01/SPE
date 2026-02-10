a03c411ba2bcc34e5d33dac53f658479bf4b1875
Leandro Andrade <leandro.andrade.401@ufrn.edu.br>
2026-02-10 13:49:05 -0300

    create of component 'login'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MainBolsistaComponent } from './components/main-bolsista/main-bolsista.component';
 import { MainAdminComponent } from './components/main-admin/main-admin.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '',
     component: HomeComponent
   },
+//mudar rotas depois.
   {
     path: 'meus-dados',
     component: MainBolsistaComponent
